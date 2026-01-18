9775060a9fb14e056a4f26bafd625c05ef05f267
小💻 <AIR@M.M>
2026-01-18 21:06:59 +0800

    更新 sidebar 折叠

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,53 +1,37 @@
 <template>
   <div class="layout">
-    <!-- 左侧菜单栏 -->
-    <aside class="sidebar">
-      <el-menu
-        router
-        default-active="/"
-        class="el-menu-vertical-demo"
-        background-color="#001529"
-        text-color="#fff"
-        active-text-color="#409EFF"
-      >
-        <!-- 首页 -->
-        <el-menu-item index="/">
-          <!-- 图标 + 文本 -->
-          <el-icon><House /></el-icon>
-          <span>首页</span>
-        </el-menu-item>
-
-        <!-- SN 查询 -->
-        <el-menu-item index="/sn">
-          <el-icon><Search /></el-icon>
-          <span>SN 查询</span>
-        </el-menu-item>
-      </el-menu>
-    </aside>
-
-    <!-- 右侧内容区 -->
-    <main class="content">
-      <router-view />
-    </main>
+    <!-- 顶部菜单栏 -->
+    <TopBar :collapsed="collapsed" @toggle="collapsed = !collapsed" />
+    <div class="body">
+      <!-- 左侧菜单栏 -->
+      <Sidebar :collapsed="collapsed" />
+      <!-- 右侧内容区 -->
+      <main class="content">
+        <router-view />
+      </main>
+    </div>
   </div>
 </template>
 
 <script setup>
 // 引入 Element Plus 图标
+import { ref } from 'vue'
 import { House, Search } from '@element-plus/icons-vue'
+import Sidebar from './components/Sidebar.vue'
+import TopBar from './components/TopBar.vue';
+const collapsed = ref(false)
 </script>
 
 <style scoped>
 .layout {
   display: flex;
+  flex-direction: column;
   height: 100vh;
 }
 
-/* 左侧菜单栏 */
-.sidebar {
-  width: 200px;
-  background-color: #001529;
-  padding-top: 20px;
+.body {
+  display: flex;
+  flex: 1;
 }
 
 /* 右侧内容区 */
---
+++ src/layouts/components/TopBar.vue
@@ -0,0 +1,72 @@
+<template>
+    <header class="topbar">
+        <div class="left">
+            <el-icon class="collapse-btn" @click="$emit('toggle')">
+                <Fold v-if="!collapsed" />
+                <Expand v-else />
+            </el-icon>
+            <span class="title">SERVER_INFO</span>
+        </div>
+
+        <div class="right">
+            <el-dropdown>
+                <span class="user">
+                    admin
+                    <el-icon>
+                        <ArrowDown />
+                    </el-icon>
+                </span>
+                <template #dropdown>
+                    <el-dropdown-menu>
+                        <el-dropdown-item>个人设置</el-dropdown-item>
+                        <el-dropdown-item divided>退出登录</el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
+        </div>
+    </header>
+</template>
+
+<script setup>
+import { ArrowDown, Fold, Expand } from '@element-plus/icons-vue'
+defineProps({
+    collapsed: Boolean
+})
+</script>
+
+<style scoped>
+.topbar {
+    height: 56px;
+    background: #1f2d3d;
+    color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+}
+
+.title {
+    font-size: 18px;
+    font-weight: bold;
+}
+
+.user {
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+}
+
+.left {
+  display: flex;
+  align-items: center; /* 图标 + 文字 垂直居中 */
+}
+
+.collapse-btn {
+  display: flex;        /* 让图标内部的 SVG 也居中 */
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  margin-right: 16px;
+  cursor: pointer;
+}
+</style>
